ab774db0fd9c15e4c1da931dd68bab4bd079ed3d
Evan Stade <estade@chromium.org>
2024-10-04 17:38:52 +0000

    Add build files to repo so it can be hosted by gh pages.

---
+++ build/precache-manifest.fb49aa74d5615a33f0a575b51eb17c39.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b4e0b3b0ac65ee252208ec4eedf85b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d238144b8218d50099c3",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "d238144b8218d50099c3",
+    "url": "/static/js/bundle.js"
+  },
+  {
+    "revision": "2716510d297d87d3ecd57bb850caaebb",
+    "url": "/static/js/bundle.js.LICENSE.txt"
+  }
+]);
