41f4fadcfaed3bd96f3eaecf2d97aac4a64b8a19
Evan Stade <estade@chromium.org>
2024-10-04 17:48:12 +0000

    undo that move

--- src/model/logs/logs_selectors.ts
+++
@@ -1,5 +0,0 @@
-import {AppState} from 'model/store';
-
-export function logsSelector(state: AppState) {
-  return state.logs;
-}
--- src/services/next_frame.ts
+++
@@ -1,9 +0,0 @@
-export function nextFrame<T>(callback: () => T): Promise<T> {
-  return new Promise((resolve) => {
-    requestAnimationFrame(() => {
-      setTimeout(() => {
-        resolve(callback());
-      }, 0);
-    });
-  });
-}
